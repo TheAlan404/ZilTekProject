6d1344e71de4801baebb452a452e9f50b27e52a3
Dennis <43997085+TheAlan404@users.noreply.github.com>
2023-12-21 08:24:48 +0000

    updates

--- relay/relay.ts
+++ relay/relay.ts
@@ -1,12 +1,15 @@
 import { Server } from "socket.io";
 import { v4, validate } from "uuid";
-import fs from "node:fs";
+import { createServer } from 'node:http';
+import express from 'express';
 import { JSONPreset } from 'lowdb/node'
 
 const defaultData = { hosts: {} }
 const db = await JSONPreset('db.json', defaultData);
 
 const PORT = Number(process.env.PORT) || 3000;
+const ADDR = process.env.ADDR || "127.0.0.1";
+const REDIRECT_TO = "https://thealan404.github.io/ziltekproject";
 
 export interface ServerToClientEvents {
 	updateState: (state: object) => void;
@@ -26,12 +29,19 @@ interface InterServerEvents {}
 
 interface SocketData {}
 
+const app = express();
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+	res.redirect(REDIRECT_TO);
+});
+
 const io = new Server<
 	ClientToServerEvents,
 	ServerToClientEvents,
 	InterServerEvents,
 	SocketData
->({
+>(server, {
 	cors: {
 		origin: "*",
 	}
@@ -94,7 +104,7 @@ io.on("connection", (socket) => {
 		io.in(`remotes-${hostId}`).emit("updateHostState", true);
 
 		socket.on("updateState", (st) => {
-			console.log(`updateState [${hostId} ==> *]`, st);
+			//console.log(`updateState [${hostId} ==> *]`, st);
 			io.to(`remotes-${hostId}`).emit("updateState", st);
 		});
 	} else {
@@ -104,7 +114,7 @@ io.on("connection", (socket) => {
 		io.in(`host-${hostId}`).emit("remoteConnected", remoteId);
 
 		socket.on("processCommand", (cmd) => {
-			console.log(`processCommand [${remoteId} ==> ${hostId}]`, cmd);
+			//console.log(`processCommand [${remoteId} ==> ${hostId}]`, cmd);
 			io.to(`host-${hostId}`).emit("processCommand", cmd);
 		});
 	}
@@ -119,6 +129,6 @@ io.on("connection", (socket) => {
 	});
 });
 
-io.listen(PORT);
-
-console.log(`Listening on port ${PORT}`);
+server.listen(PORT, ADDR, () => {
+	console.log(`Listening on addr ${ADDR}, port ${PORT}`);
+});
